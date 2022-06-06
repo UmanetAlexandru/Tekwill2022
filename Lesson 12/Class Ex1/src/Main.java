e684ffc0b92d7126af3f86a62031cecdc36dcba8
Alexandru Umanet <alexandru.umanet@gmail.com>
2022-06-06 20:36:03 +0300

    - lesson 12

---
+++ Lesson 12/Class Ex1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Employee e = new Employee("Alex", "2000");
+        ContractEmployee ce = new ContractEmployee("Alex", "2000", 200, "");
+        RegularEmployee re = new RegularEmployee("Alex", "2000", 20000, "");
+    }
+}
---
+++ Lesson 12/Class Ex2/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Duck d = new Duck();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        a.say();
+        d.say();
+        dog.say();
+        cat.say();
+    }
+}
